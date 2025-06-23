67f65e57a6ad990e163aa4754dfee8e8a3f6dbc0
Shlok Khandelwal <u24ec044@eced.svnit.ac.in>
2025-06-24 03:26:25 +0530

    Create grey_encoder_4bit.v

---
+++ encoder/grey_encoder/4bit/grey_encoder_4bit.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module grey_encoder_4bit(
+    input [3:0] i_bin,
+    input i_en,
+    output reg [3:0] o_grey
+    );
+    
+    always @(*) begin
+        if (i_en) begin
+            o_grey[3] = i_bin[3];
+            o_grey[2] = i_bin[3] ^ i_bin[2];
+            o_grey[1] = i_bin[2] ^ i_bin[1];
+            o_grey[0] = i_bin[1] ^ i_bin[0];
+        end
+        else begin
+            o_grey = 4'bxxxx;
+        end
+    end
+endmodule

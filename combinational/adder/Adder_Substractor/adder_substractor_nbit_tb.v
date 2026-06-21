5ff50b531ca7f53724424d06f367d3056cf8e24f
Shlok Khandelwal <u24ec044@eced.svnit.ac.in>
2026-06-21 18:51:35 +0530

    Remove header comments in adder_substractor_nbit_tb.v
    Strip the auto-generated boilerplate file header from the adder_substractor_nbit_tb.v testbench, leaving only the `timescale` directive and the module declaration. This is a non-functional cleanup to reduce noise in the source file.

--- combinational/adder/Adder_Substractor/adder_substractor_nbit_tb.v
+++ combinational/adder/Adder_Substractor/adder_substractor_nbit_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.06.2025 02:07:33
-// Design Name: 
-// Module Name: adder_substractor_nbit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module adder_substractor_nbit_tb(

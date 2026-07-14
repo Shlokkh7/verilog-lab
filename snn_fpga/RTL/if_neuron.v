cc97a85d00b45be1c60dbbe17dfdb2c5512b06d4
Shlok Khandelwal <u24ec044@eced.svnit.ac.in>
2026-07-14 21:57:17 +0530

    Add Vivado project scaffolding
    Add a `Projects` workspace with Vivado ignore rules and placeholder build/run files, and introduce an initial `if_neuron` RTL module stub under `snn_fpga/RTL`.

---
+++ snn_fpga/RTL/if_neuron.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.07.2026 16:07:54
+// Design Name: 
+// Module Name: if_neuron
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module if_neuron(
+
+    );
+endmodule

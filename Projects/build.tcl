ad4e6fbc543ba4965286de21f64fad84f4aa63bd
Shlok Khandelwal <u24ec044@eced.svnit.ac.in>
2026-07-28 17:07:24 +0530

    Update build.tcl

--- Projects/build.tcl
+++ Projects/build.tcl
@@ -4,16 +4,28 @@ set src_dir    "./src"
 set tb_dir     "./tb"
 set constr_dir "./constraints"
 
-if {![file exists $proj_dir]} {
-    create_project auto_proj $proj_dir -part YOUR_PART_NUMBER -force
+if {![file exists $proj_dir/auto_proj.xpr]} {
+    create_project auto_proj $proj_dir -part xc7z020clg484-1 -force
 } else {
     open_project $proj_dir/auto_proj.xpr
 }
 
 remove_files [get_files] -quiet
-add_files [glob -nocomplain $src_dir/*.v $src_dir/*.sv]
-add_files -fileset sim_1 [glob -nocomplain $tb_dir/*.v $tb_dir/*.sv]
-add_files -fileset constrs_1 [glob -nocomplain $constr_dir/*.xdc]
+
+set src_files [glob -nocomplain $src_dir/*.v $src_dir/*.sv]
+if {[llength $src_files] > 0} {
+    add_files $src_files
+}
+
+set tb_files [glob -nocomplain $tb_dir/*.v $tb_dir/*.sv]
+if {[llength $tb_files] > 0} {
+    add_files -fileset sim_1 $tb_files
+}
+
+set constr_files [glob -nocomplain $constr_dir/*.xdc]
+if {[llength $constr_files] > 0} {
+    add_files -fileset constrs_1 $constr_files
+}
 
 set_property top $top_module [current_fileset]
 update_compile_order -fileset sources_1

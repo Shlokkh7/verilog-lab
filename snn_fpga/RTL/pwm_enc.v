85003c09c933b03001978f148890eb2540271b14
Shlok Khandelwal <u24ec044@eced.svnit.ac.in>
2026-08-08 00:36:53 +0530

    Initialize snn_fpga Vivado project scaffold
    Add the initial `snn_fpga` project structure, including a Vivado `.xpr` configuration targeting `xc7k70tfbv676-1`, a starter `RTL/pwm_enc.v` module shell, and a project-level `.gitignore` to exclude Vivado-generated artifacts from version control.

---
+++ snn_fpga/RTL/pwm_enc.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.08.2026 00:31:08
+// Design Name: 
+// Module Name: pwm_enc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_enc(
+
+    );
+endmodule
